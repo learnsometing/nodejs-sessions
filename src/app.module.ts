071492c19ca902d9ac9c59549c3cecd66e1f2019
Brian Monaccio <brianmonaccio@gmail.com>
2021-04-02 18:35:51 -0400

    Establishes dev and prod database connections.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// Config
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+// Mongoose
+import { MongooseModule } from '@nestjs/mongoose';
+
+const ENV = process.env.NODE_ENV;
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+    }),
+    MongooseModule.forRoot(process.env.DB_URL, { useNewUrlParser: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {
+    const db = this.configService.get<string>('DB_URL');
+    console.log('Connected to: ', db);
+  }
+}
