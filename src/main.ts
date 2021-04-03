35d959506f6648d1e8d8c24edfeb7fe18f96292e
Brian Monaccio <brianmonaccio@gmail.com>
2021-04-03 19:01:50 -0400

    Configures OpenAPI documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// OpenAPI
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('nodejs-sessions')
+    .setDescription('Nodejs User Sessions API')
+    .setVersion('0.0.0')
+    .addTag('user-sessions')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
