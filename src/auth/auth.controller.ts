f439f12fa71553ab9298017ac320a269686ee361
Brian Monaccio <brianmonaccio@gmail.com>
2021-04-05 23:04:16 -0400

    Implements GET /auth/account route for returning serialized user id

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Request,
+  Response,
+  UseGuards,
+} from '@nestjs/common';
 
 // Passport Auth Guards
+import { AuthenticatedGuard } from '../common/guards/authenticated.guard';
 import LocalAuthGuard from './local-auth.guard';
 
 // Swagger
@@ -10,6 +18,8 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 
+// Types
+import { User } from '../users/schemas/user.schema';
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
@@ -22,8 +32,18 @@ export class AuthController {
     description: 'Internal server error',
   })
   @Post('/login')
-  async login() {
-    return { message: 'Login successful' };
+  async login(@Response() res) {
+    res.redirect('/auth/account');
+  }
+
+  @ApiOkResponse({
+    description: "Get the currently logged in user's account.",
+    type: [User],
+  })
+  @UseGuards(AuthenticatedGuard)
+  @Get('/account')
+  getAccount(@Request() req, @Response() res) {
+    res.redirect('/users/' + req.user);
   }
 
   @ApiOkResponse({
